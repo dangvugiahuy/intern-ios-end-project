ff15bf5a5cd30338d0796c99176dc26c42d9043d
Gia Huy <dangvugiahuy.2002@gmail.com>
2024-09-10 17:10:37 +0700

    first setup project

---
+++ Remind Me/Remind Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remind Me
+//
+//  Created by huy.dang on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
