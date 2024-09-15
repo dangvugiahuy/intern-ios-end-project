7b276908a76133a9bca7a5398881ce4b9a5181d4
Huy Gia <dangvugiahuy.2002@gmail.com>
2024-09-15 20:51:35 +0700

    ProfileView (temponary)

---
+++ Remind Me/Remind Me/Views/ProfileFlow/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Remind Me
+//
+//  Created by Huy Gia on 14/9/24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
